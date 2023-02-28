598835b5ff820ebc7c45a6d198ecc6c84bd5ffb4
aiiro-s <deepblueman0305@gmail.com>
2023-03-01 02:06:13 +0900

    test:github_serviceクラスのUnitテストを追加

---
+++ test/service/github_service_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_codecheck/Service/github_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  test('GitHubAPIのアクセス確認', () async {
+    final service = GithubService();
+    const query = 'Q';
+    final response = await service.repositoryFetch(query);
+    expect(response, isA<http.Response>());
+    expect(response.statusCode, 200);
+  });
+}
