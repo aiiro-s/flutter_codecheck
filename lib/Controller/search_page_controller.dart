04fa8562e1e2d33fc64881f30b9edb08eecf4597
aiiro-s <deepblueman0305@gmail.com>
2023-03-01 23:34:30 +0900

    add:SearchPageControllerクラスの追加

---
+++ lib/Controller/search_page_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_codecheck/Entities/repository.dart';
+import 'package:flutter_codecheck/Repository/repo_repository.dart';
+import 'package:riverpod/riverpod.dart';
+
+final searchPageControllerProvider =
+    StateNotifierProvider<SearchPageController, Repository>((ref) {
+  return SearchPageController(ref.read(repoRepositoryProvider));
+});
+
+class SearchPageController extends StateNotifier<Repository> {
+  SearchPageController(this.repoRepository)
+      : super(const Repository(total_count: 0, items: null));
+  RepoRepository repoRepository;
+
+  Future<Repository> fetch(String keyword) async {
+    state = await repoRepository.fetchByKeyword(keyword);
+    return state.copyWith();
+  }
+}
