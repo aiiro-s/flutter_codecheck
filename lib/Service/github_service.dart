d0ee332c543010b3235b01e7e6deedfb7db3607a
aiiro-s <deepblueman0305@gmail.com>
2023-03-01 23:33:39 +0900

    add:serviceProviderの追加

--- lib/Service/github_service.dart
+++ lib/Service/github_service.dart
@@ -1,4 +1,7 @@
 import 'package:http/http.dart' as http;
+import 'package:riverpod/riverpod.dart';
+
+final serviceProvider = Provider((ref) => GithubService());
 
 class GithubService {
   Future<http.Response> repositoryFetch(String query) async {
