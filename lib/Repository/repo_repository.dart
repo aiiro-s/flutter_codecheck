64c8dea2b7b9e5ad069b0edf3b13b714bc124662
aiiro-s <deepblueman0305@gmail.com>
2023-03-02 20:23:50 +0900

    fix:RiverpodのパッケージをFlutter版に修正

--- lib/Repository/repo_repository.dart
+++ lib/Repository/repo_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_codecheck/Entities/repository.dart';
 import 'package:flutter_codecheck/Repository/repo_repository_impl.dart';
 import 'package:flutter_codecheck/Service/github_service.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final repoRepositoryProvider = Provider<RepoRepository>(
     (ref) => RepoRepositoryImpl(ref.read(serviceProvider)));
